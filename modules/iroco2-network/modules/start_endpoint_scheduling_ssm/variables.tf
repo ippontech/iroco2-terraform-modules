4f4d21a100767fc4768a0e3a9d3af0cc1f41124c
donovan <donovanhoang.pro@gmail.com>
2025-11-05 21:16:03 +0100

    feat: move all resources from network layer into a dedicated submodule

---
+++ modules/iroco2-network/modules/start_endpoint_scheduling_ssm/variables.tf
@@ -0,0 +1,63 @@
+# Copyright 2025 Ippon Technologies
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Common
+variable "namespace" {
+  type        = string
+  description = "The namespace in which the project is."
+}
+
+variable "SecurityGroupId" {
+  type        = string
+  description = "The id of the security group"
+}
+
+variable "SubnetIds" {
+  type        = list(any)
+  description = "List of subnet ids"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The id of the VPC to use."
+}
+
+variable "RouteTableIds" {
+  type        = list(string)
+  description = "The list of route table Id"
+}
+
+# SSM
+
+variable "tag_autoshutdown" {
+  type        = string
+  description = "Describe how the ressources is managed: True if a ssm document is applied to start and stop the ressource"
+}
+
+variable "endpoint" {
+  type        = any
+  description = "The endpoint we want to automate"
+}
+
+variable "ssm_document" {
+  description = "SSM document's name to apply"
+  type        = string
+}
+
+variable "working_days" {
+  type        = list(string)
+  description = "The list of working days"
+}
