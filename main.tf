794b094ce355a47bc3b70f56768c43d073ee4912
donovan <donovanhoang.pro@gmail.com>
2025-11-05 21:26:54 +0100

    feat: include the network submodule into the root module

---
+++ main.tf
@@ -0,0 +1,10 @@
+module "iroco2-network" {
+    source = "./modules/iroco2-network"
+
+    environment = var.environment
+    domain_name = var.domain_name
+    cidr        = var.cidr
+    private_subnets = var.private_subnets
+    public_subnets  = var.public_subnets
+    database_subnets = var.database_subnets
+}
