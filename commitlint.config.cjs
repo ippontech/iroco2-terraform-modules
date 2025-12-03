22c12394aa263ced4ded4d7f55b339e41db29c0d
Axel Mesnard <amesnard@ippon.fr>
2025-12-03 15:15:17 +0100

    chore: extend commitlint rules with custom line length limits
    Adds custom rules to allow longer commit headers and bodies to accommodate
    more detailed commit messages while maintaining readability standards

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-  };
+    rules: {
+        'body-max-line-length': [2, 'always', 400],
+        'header-max-length': [2, 'always', 120],
+    },
+};
